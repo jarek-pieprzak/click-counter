a1677780e81e235a2de7f11aeb9e5e0e981d524a
Jarosław Pieprzak <33102947+jarek-pieprzak@users.noreply.github.com>
2018-01-01 14:18:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  incHandler = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  decHandler = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.decHandler()}>-</button>
+        <button onClick={this.incHandler()}>+</button>
+      </div>
+    );
+  }
+}
+
+export default App;
